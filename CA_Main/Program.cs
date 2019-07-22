f8c897dd443a3a828062897a14fe4a2a24f5c0db
Hikari Genzo <leonxp@libero.it>
2019-07-22 09:30:51 +0200

    First Commit

---
+++ CA_Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
