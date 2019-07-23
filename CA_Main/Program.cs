8c92c05332c7ed5b7754d065d8ecd789befcb67c
Hikari Genzo <leonxp@libero.it>
2019-07-24 00:11:33 +0200

    Just Log

--- CA_Main/Program.cs
+++ CA_Main/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
+using Serilog.Core;
+using Serilog;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace CA_Main
 {
     class Program
     {
+        public static string appName = "MinicapReceiver.Core";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logger logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithThreadId()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} | {Level} | {ThreadId} | {Method} >>>> {Message}{NewLine}{Exception}", theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            try
+            {                
+                logger.Information(string.Format("Starting Application {0} ...", appName));
+
+                Console.WriteLine("Hello World!");
+            }
+            catch(Exception ex)
+            {
+                logger.Fatal(ex, string.Format("An Error Occurred! Message: {0}", ex.Message));
+            }
+            finally
+            {
+                stopwatch.Stop();
+                logger.Information(string.Format("Application {0} completed in {1}", appName, Utils.ElapsedTime(stopwatch.Elapsed)));
+            }
         }
     }
 }
---
+++ CA_Main/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA_Main
+{
+    public static class Utils
+    {
+        public static string ElapsedTime(TimeSpan ts)
+        {
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000} (hh:mm:ss.mmm)", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            return elapsedTime;
+        }
+    }
+}
